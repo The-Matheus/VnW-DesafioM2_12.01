052924fed0c4cd1773bbb1e305d4c1746a825560
The-Matheus <matheusaguiar18@outlook.com>
2022-01-21 17:58:13 -0300

    Desafio de lógica 01 - Estrutura de dados

---
+++ script.js
@@ -0,0 +1,31 @@
+// Crie uma variável que receba um nome;
+
+let name = "Matheus"
+
+// Crie uma variável que receba um número;
+
+let age = 24
+
+// Crie uma variável com o ano atual e subtraia pelo número que você guardou;
+
+let year = 2022
+let past = year - age
+console.log(past)
+
+// Agora mostre no console o resultado da soma das duas variáveis guardadas anteriormente;
+
+let sum = year + past
+console.log(sum)
+
+//Mostre no console a mensagem "Estou mexendo no console.log";
+
+console.log("Estou mexendo no console.log")
+
+// Crie uma variável chamada “quartaFeira” e atribua à ela o valor booleano que representa verdadeiro ou falso e mostre a saída no console;
+
+let quartaFeira = false
+console.log(quartaFeira)
+
+// Exiba no console o tipo de dado da váriavel quartaFeira;
+
+console.log(typeof(quartaFeira))
